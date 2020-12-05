3e8cf5071a9abed7e602ad597dc95354a09be67e
David Mendonça <davidmed66@gmail.com>
2020-12-05 00:08:46 -0300

    adição de icone no botão delete e outras coisas de css

--- js/funcoes.js
+++ js/funcoes.js
@@ -11,22 +11,23 @@ function criarTabela() {
                 "<th>Item</th>" +
                 "<th>Excluir</th>" +
             "</tr>"+
-        "</table>");
+        "</table>"
+    );
     
-        for(id = 0; id < localStorage.length; id++) {
-                $("#tabela").append(
-                    "<tr id = 'row" + id + "'>" +
-                        "<td> <input type='checkbox' id = 'checkbox" + id + "'" + 
-                        "onclick='lineThrough( " + id + ")'" + " value = ''></td>" +
-                        "<td id = 'linha" + id + "'>" + localStorage.getItem(id) + "</td>" +
-                        "<td><button type = 'button' id = 'botao" + id + "'" + 
-                        "onclick = 'deletaLinha( " + id + ")'" + ">Excluir</button></td>" +
-                    "</tr>"
-                );
-                var idToString ="#linha" + id.toString();
-                $(idToString).css("width", "300px");
-            
-        }   
+    for(id = 0; id < localStorage.length; id++) {
+        $("#tabela").append(
+            "<tr id = 'row" + id + "'>" +
+                "<td> <input type='checkbox' id = 'checkbox" + id + "'" + 
+                "onclick='lineThrough( " + id + ")'" + " value = 'false'></td>" +
+                "<td id = 'linha" + id + "'>" + localStorage.getItem(id) + "</td>" +
+                "<td><button type = 'button' class='botao' id = 'botao" + id + "'" + 
+                "onclick = 'deletaLinha( " + id + ")'>"+
+                "<img src='assets/lixocinza.ico'></button></td>" +
+            "</tr>"
+        );
+        var idToString ="#linha" + id.toString();
+        $(idToString).css("width", "300px");
+    }   
 }
 
 $("#btn").click(function enviaDado() {
@@ -43,13 +44,11 @@ function lineThrough() {
     var idCheckBox = "#checkbox" + pegaId;
     var linha = "#linha" + pegaId;
     
-    if(localStorage.key(idCheckBox) == false) {
-        alert('entrou no false')
+    if($(idCheckBox).val() == "false") {
         $(linha).css("text-decoration", "line-through");
         $(idCheckBox).val("true");
         
     } else {
-        alert('entrou no true')
         $(linha).css("text-decoration", "none");
         $(idCheckBox).val("false");
         
@@ -57,14 +56,12 @@ function lineThrough() {
 }
 
 function deletaLinha() {
-    var pegaId = arguments[0];
-    pegaId.toString();
+    var pegaId = arguments[0].toString();
     var item = localStorage.getItem(pegaId)
     var linha = "#row" + pegaId;
     var resposta = confirm("Deseja realmente excluir o item: " + item + "?");
     if(resposta == true) {
         localStorage.removeItem(pegaId);
         $(linha).hide();
-        
     }
 }
