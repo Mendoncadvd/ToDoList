e5fa728879a6cc06bd006e068364cb066eec835d
David Mendonça <davidmed66@gmail.com>
2020-12-04 21:21:10 -0300

    Correção do problema que aparecia null

--- js/funcoes.js
+++ js/funcoes.js
@@ -14,10 +14,10 @@ function criarTabela() {
         "</table>");
     
         for(id = 0; id < localStorage.length; id++) {
-            if(localStorage.key(id)!=null) {
                 $("#tabela").append(
                     "<tr id = 'row" + id + "'>" +
-                        "<td> <input type='checkbox' id = '" + id + "'" + "onclick='lineThrough( " + id + ")'" + "></td>" +
+                        "<td> <input type='checkbox' id = 'checkbox" + id + "'" + 
+                        "onclick='lineThrough( " + id + ")'" + " value = ''></td>" +
                         "<td id = 'linha" + id + "'>" + localStorage.getItem(id) + "</td>" +
                         "<td><button type = 'button' id = 'botao" + id + "'" + 
                         "onclick = 'deletaLinha( " + id + ")'" + ">Excluir</button></td>" +
@@ -25,13 +25,13 @@ function criarTabela() {
                 );
                 var idToString ="#linha" + id.toString();
                 $(idToString).css("width", "300px");
-            } 
+            
         }   
 }
 
 $("#btn").click(function enviaDado() {
     var usuarioInput = $("#listInput").val();
-    id = localStorage.length;
+    var id = localStorage.length;
     localStorage.setItem(id.toString(), usuarioInput); 
     criarTabela();
     $("#listInput").val('');
@@ -40,18 +40,31 @@ $("#btn").click(function enviaDado() {
 function lineThrough() {
     var pegaId = arguments[0];
     pegaId.toString();
+    var idCheckBox = "#checkbox" + pegaId;
     var linha = "#linha" + pegaId;
-    $(linha).css("text-decoration", "line-through");
+    
+    if(localStorage.key(idCheckBox) == false) {
+        alert('entrou no false')
+        $(linha).css("text-decoration", "line-through");
+        $(idCheckBox).val("true");
+        
+    } else {
+        alert('entrou no true')
+        $(linha).css("text-decoration", "none");
+        $(idCheckBox).val("false");
+        
+    }
 }
 
 function deletaLinha() {
     var pegaId = arguments[0];
-    /* pegaId.toString(); */
+    pegaId.toString();
     var item = localStorage.getItem(pegaId)
-    /* var linha = "#row" + pegaId; */
+    var linha = "#row" + pegaId;
     var resposta = confirm("Deseja realmente excluir o item: " + item + " da id " + pegaId +"?");
     if(resposta == true) {
         localStorage.removeItem(pegaId);
-        criarTabela();
+        $(linha).hide();
+        
     }
 }
