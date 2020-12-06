3a606a7e31c6106aee28fa2ed8d1df6602ffd38e
David Mendonça <davidmed66@gmail.com>
2020-12-05 22:33:03 -0300

    Mais corre

--- js/funcoes.js
+++ js/funcoes.js
@@ -16,12 +16,12 @@ function criarTabela() {
     var arrayKeys = Object.keys(localStorage);
     var arrayOrdenado = ordenaArray(arrayKeys);
     for (var id = 0; id < localStorage.length; id++) {
-        var item = localStorage.getItem(arrayOrdenado[id]);
+        var item = JSON.parse(localStorage.getItem(arrayOrdenado[id]));
         $("#tabela").append(
             "<tr id = 'row" + arrayOrdenado[id] + "'>" +
             "<td> <input type='checkbox' id = 'checkbox" + arrayOrdenado[id] + "'" +
-            "onclick='lineThrough( " + arrayOrdenado[id] + ")'" + " value = 'false'></td>" +
-            "<td id = 'linha" + arrayOrdenado[id] + "'>" + item + "</td>" +
+            "onclick='lineThrough( " + arrayOrdenado[id] + ")'" + " value = '"+ item.check +"'></td>" +
+            "<td id = 'linha" + arrayOrdenado[id] + "'>" + item.input + "</td>" +
             "<td><button type = 'button' class='botao' id = 'botao" + arrayOrdenado[id] + "'" +
             "onclick = 'deletaLinha( " + arrayOrdenado[id] + ")'>" +
             "<img src='assets/lixocinza.ico'></button></td>" +
@@ -29,38 +29,47 @@ function criarTabela() {
         );
         var idToString = "#linha" + arrayOrdenado[id];
         $(idToString).css("width", "300px");
+        var linha = "#linha" + arrayOrdenado[id].toString();
+        var idCheckbox = "#checkbox" + arrayOrdenado[id];
+        if (item.check == true) {
+            $(linha).css("text-decoration", "line-through");
+            $(idCheckbox).attr("checked", "checked");
+        }else {
+            $(linha).css("text-decoration", "none");
+        }
     }
 }
 
 $("#btn").click(function enviaDado() {
     var usuarioInput = $("#listInput").val();
-    var id = localStorage.length;
-    localStorage.setItem(id.toString(), usuarioInput);
+    var id = localStorage.length + 1;
+    var dados = {input: usuarioInput, check:false};
+    localStorage.setItem(id.toString(), JSON.stringify(dados));
     criarTabela();
     $("#listInput").val('');
 });
 
 function lineThrough() {
     var pegaId = arguments[0];
-    pegaId.toString();
-    var idCheckBox = "#checkbox" + pegaId;
-    var linha = "#linha" + pegaId;
-
+    var idCheckBox = "#checkbox" + pegaId.toString();
+    var item = JSON.parse(localStorage.getItem(pegaId));
+    var usuarioInput = item.input;
+    
     if ($(idCheckBox).val() == "false") {
-        $(linha).css("text-decoration", "line-through");
-        $(idCheckBox).val("true");
-
+        item = {input: usuarioInput, check: true};
+        localStorage.setItem(pegaId.toString(), JSON.stringify(item));
     } else {
-        $(linha).css("text-decoration", "none");
-        $(idCheckBox).val("false");
-
+        item = {input: usuarioInput, check: false};
+        localStorage.setItem(pegaId.toString(), JSON.stringify(item));
     }
-}
+    criarTabela();
+    
+} 
 
 function deletaLinha() {
     var pegaId = arguments[0].toString();
-    var item = localStorage.getItem(pegaId)
-    var resposta = confirm("Deseja realmente excluir o item: " + item + "?");
+    var item = JSON.parse(localStorage.getItem(pegaId));
+    var resposta = confirm("Deseja realmente excluir o item: " + item.input + "?");
     if (resposta == true) {
         localStorage.removeItem(pegaId);
         criarTabela();
